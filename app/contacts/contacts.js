0ca4fc8e11918f5b91e8fe322ff623046581ae76
akshay <pandeya@vmcmail.com>
2017-07-27 11:16:25 -0500

    buttons added fb failing

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -11,21 +11,31 @@ angular.module('myContacts.contacts', ['ngRoute','firebase'])
 
 .controller('ContactsCtrl', ['$scope', '$firebaseArray' ,function($scope,$firebaseArray) {
     
+// 
+//  var config = {
+//    apiKey: "AIzaSyD7YVyOR1RNxzDFRfx9o8NwfAOkF4Xtrrs",
+//    authDomain: "mycontacts-b0419.firebaseapp.com",
+//    databaseURL: "https://mycontacts-b0419.firebaseio.com",
+//    projectId: "mycontacts-b0419",
+//    storageBucket: "mycontacts-b0419.appspot.com",
+//    messagingSenderId: "412513302790"
+//  };
+//  firebase.initializeApp(config);
+//  console.log('firebase authenticated successfully');    
  
-  var config = {
-    apiKey: "AIzaSyD7YVyOR1RNxzDFRfx9o8NwfAOkF4Xtrrs",
-    authDomain: "mycontacts-b0419.firebaseapp.com",
-    databaseURL: "https://mycontacts-b0419.firebaseio.com",
-    projectId: "mycontacts-b0419",
-    storageBucket: "mycontacts-b0419.appspot.com",
-    messagingSenderId: "412513302790"
-  };
-  firebase.initializeApp(config);
-  console.log('firebase authenticated successfully');    
- 
-  var ref = firebase.database().ref();
+//  var ref = firebase.database().ref();
+//    
+//  $scope.contacts = $firebaseArray(ref);
     
-  $scope.contacts = $firebaseArray(ref);
+  $scope.showAddForm = function(){
+      
+      $scope.addFormShow = true;
+  }
+  
+  $scope.hide = function(){
+      
+      $scope.addFormShow = false;
+  }
     
 
 
